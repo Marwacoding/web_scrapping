f2c8209aa5b532e061e2be8a831a655f77878620
Marwa <majbrim@gmail.com>
2021-03-11 11:46:20 +0100

    postgress error, commenting issue

--- flask_api.py
+++ flask_api.py
@@ -21,11 +21,11 @@ CORS(app)
 #                                 )
 conn = psycopg2.connect(
 
-    host = os.environ["host"]
-    dbname = os.environ["my_db"]
-    user = os.environ["user"]
-    password = os.environ["pw"]
-    sslmode = "require" 
+    host = os.environ["host"],
+    dbname = os.environ["db"],
+    user = os.environ["user"],
+    password = os.environ["pw"],
+    sslmode = "require", 
 
     )
 
@@ -65,81 +65,81 @@ def api_mirror():
     output = sql_query.fetchall()
     return jsonify(output)
 
-@app.route('/api_carpet_lowest_to_highest_price', methods=['GET', 'POST'])
-def carpet_lowest_to_highest_price():
+# @app.route('/api_carpet_lowest_to_highest_price', methods=['GET', 'POST'])
+# def carpet_lowest_to_highest_price():
 
-    #https://stackoverflow.com/questions/8887760/order-by-price-returns-a-weird-order-in-mysql
+#     #https://stackoverflow.com/questions/8887760/order-by-price-returns-a-weird-order-in-mysql
 
-    sql_query.execute(f"SELECT * FROM carpet ORDER BY CAST(carpet_price AS DECIMAL(10,2)) ASC")
-    output = sql_query.fetchall()
-    return jsonify(output)
+#     sql_query.execute(f"SELECT * FROM carpet ORDER BY CAST(carpet_price AS DECIMAL(10,2)) ASC")
+#     output = sql_query.fetchall()
+#     return jsonify(output)
 
-@app.route('/api_carpet_highest_to_lowest_price', methods=['GET', 'POST'])
-def carpet_highest_to_lowest_price():
+# @app.route('/api_carpet_highest_to_lowest_price', methods=['GET', 'POST'])
+# def carpet_highest_to_lowest_price():
 
-    sql_query.execute(f"SELECT * FROM carpet ORDER BY CAST(carpet_price AS DECIMAL(10,2)) DESC")
-    output = sql_query.fetchall()
-    return jsonify(output)
+#     sql_query.execute(f"SELECT * FROM carpet ORDER BY CAST(carpet_price AS DECIMAL(10,2)) DESC")
+#     output = sql_query.fetchall()
+#     return jsonify(output)
 
-@app.route('/api_mirror_lowest_to_highest_price', methods=['GET', 'POST'])
-def mirror_lowest_to_highest_price():
+# @app.route('/api_mirror_lowest_to_highest_price', methods=['GET', 'POST'])
+# def mirror_lowest_to_highest_price():
 
-    sql_query.execute(f"SELECT * FROM mirror ORDER BY CAST(mirror_price AS DECIMAL(10,2)) ASC")
-    output = sql_query.fetchall()
-    return jsonify(output)
+#     sql_query.execute(f"SELECT * FROM mirror ORDER BY CAST(mirror_price AS DECIMAL(10,2)) ASC")
+#     output = sql_query.fetchall()
+#     return jsonify(output)
 
-@app.route('/api_mirror_highest_to_lowest_price', methods=['GET', 'POST'])
-def mirrro_highest_to_lowest_price():
+# @app.route('/api_mirror_highest_to_lowest_price', methods=['GET', 'POST'])
+# def mirrro_highest_to_lowest_price():
 
-    sql_query.execute(f"SELECT * FROM mirror ORDER BY CAST(mirror_price AS DECIMAL(10,2)) DESC")
-    output = sql_query.fetchall()
-    return jsonify(output)
+#     sql_query.execute(f"SELECT * FROM mirror ORDER BY CAST(mirror_price AS DECIMAL(10,2)) DESC")
+#     output = sql_query.fetchall()
+#     return jsonify(output)
 
-# URL A SAISIR == http://localhost:4050/api_carpet_price?name=carpet&price=200
-@app.route('/api_carpet_price', methods=['GET', 'POST'])
-def carpet_price():
+# # URL A SAISIR == http://localhost:4050/api_carpet_price?name=carpet&price=200
+# @app.route('/api_carpet_price', methods=['GET', 'POST'])
+# def carpet_price():
 
-    carpet_name = request.args.get('name')
-    carpet_price = request.args.get('price')
-
-
-    sql_query.execute(f"SELECT * FROM {carpet_name} WHERE carpet_price <= {carpet_price}")
-    output = sql_query.fetchall()
-    return jsonify(output)
-
-@app.route('/api_mirror_price', methods=['GET', 'POST'])
-def mirror_price():
-    mirror_name = request.args.get('name')
-    mirror_price = request.args.get('price')
-
-    sql_query.execute(f"SELECT * FROM {mirror_name} WHERE mirror_price <= {mirror_price}")
-    output = sql_query.fetchall()
-    return jsonify(output)
-
-@app.route('/api_carpet_texture', methods=['GET', 'POST'])
-def type_carpet():
-    carpet_desc = request.args.get('matiere')
-
-    sql_query.execute(f'SELECT * FROM carpet WHERE carpet_description LIKE "%{carpet_desc}%" ')
-    output = sql_query.fetchall()
-    return jsonify(output)
+#     carpet_name = request.args.get('name')
+#     carpet_price = request.args.get('price')
 
 
-@app.route('/api_mirror_type', methods=['GET', 'POST'])
-def type_mirror():
-    mirror_type = request.args.get('type')
+#     sql_query.execute(f"SELECT * FROM {carpet_name} WHERE carpet_price <= {carpet_price}")
+#     output = sql_query.fetchall()
+#     return jsonify(output)
 
-    sql_query.execute(f'SELECT * FROM mirror WHERE mirror_description LIKE "%{mirror_type}%" ')
-    output = sql_query.fetchall()
-    return jsonify(output)
+# @app.route('/api_mirror_price', methods=['GET', 'POST'])
+# def mirror_price():
+#     mirror_name = request.args.get('name')
+#     mirror_price = request.args.get('price')
 
-@app.route('/api_mirror_color', methods=['GET', 'POST'])
-def color_mirror():
-    mirror_color = request.args.get('color')
+#     sql_query.execute(f"SELECT * FROM {mirror_name} WHERE mirror_price <= {mirror_price}")
+#     output = sql_query.fetchall()
+#     return jsonify(output)
 
-    sql_query.execute(f'SELECT * FROM mirror WHERE mirror_description LIKE "%{mirror_color}%" ')
-    output = sql_query.fetchall()
-    return jsonify(output)
+# @app.route('/api_carpet_texture', methods=['GET', 'POST'])
+# def type_carpet():
+#     carpet_desc = request.args.get('matiere')
+
+#     sql_query.execute(f'SELECT * FROM carpet WHERE carpet_description LIKE "%{carpet_desc}%" ')
+#     output = sql_query.fetchall()
+#     return jsonify(output)
+
+
+# @app.route('/api_mirror_type', methods=['GET', 'POST'])
+# def type_mirror():
+#     mirror_type = request.args.get('type')
+
+#     sql_query.execute(f'SELECT * FROM mirror WHERE mirror_description LIKE "%{mirror_type}%" ')
+#     output = sql_query.fetchall()
+#     return jsonify(output)
+
+# @app.route('/api_mirror_color', methods=['GET', 'POST'])
+# def color_mirror():
+#     mirror_color = request.args.get('color')
+
+#     sql_query.execute(f'SELECT * FROM mirror WHERE mirror_description LIKE "%{mirror_color}%" ')
+#     output = sql_query.fetchall()
+#     return jsonify(output)
 
 @app.route('/href', methods=['GET'])
 def links():
